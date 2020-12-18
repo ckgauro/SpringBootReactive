151150672ebc7eed0e55e5b4c9aa8fd0a6578f52
Chandra kishor Gauro <ckgauro@gmail.com>
2020-12-18 17:35:49 -0500

    init

---
+++ src/main/java/com/learnreactspring/LearnReactSpringApplication.java
@@ -0,0 +1,13 @@
+package com.learnreactspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnReactSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnReactSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnreactspring/LearnReactSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnreactspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnReactSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
