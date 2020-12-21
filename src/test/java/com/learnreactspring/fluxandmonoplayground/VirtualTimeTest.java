d7dbc473f1762e71e70927df38ccb7b8795b401f
Chandra kishor Gauro <ckgauro@gmail.com>
2020-12-20 19:26:39 -0500

    Section 7

---
+++ src/test/java/com/learnreactspring/fluxandmonoplayground/VirtualTimeTest.java
@@ -0,0 +1,41 @@
+package com.learnreactspring.fluxandmonoplayground;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
+
+import java.time.Duration;
+
+/**
+ * @author Chandra
+ */
+public class VirtualTimeTest {
+    @Test
+    public void testingWithoutVirtualTime(){
+
+        Flux<Long> longFlux = Flux.interval(Duration.ofSeconds(1))
+                .take(3);
+
+        StepVerifier.create(longFlux.log())
+                .expectSubscription()
+                .expectNext(0l,1l,2l)
+                .verifyComplete();
+    }
+
+    @Test
+    public void testingWithVirtualTime(){
+
+        VirtualTimeScheduler.getOrSet();
+
+        Flux<Long> longFlux = Flux.interval(Duration.ofSeconds(1))
+                .take(3);
+
+        StepVerifier.withVirtualTime(() -> longFlux.log())
+                .expectSubscription()
+                .thenAwait(Duration.ofSeconds(3))
+                .expectNext(0l,1l,2l)
+                .verifyComplete();
+
+    }
+}
