9ad2742317bc62b4355993fa617e42a1f24055c8
Chandra kishor Gauro <ckgauro@gmail.com>
2020-12-23 09:57:30 -0500

    section 9 Lect 33

---
+++ src/main/java/com/learnreactspring/router/RouterFunctionConfig.java
@@ -0,0 +1,25 @@
+package com.learnreactspring.router;
+
+import com.learnreactspring.handler.SampleHandlerFunction;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
+/**
+ * @author Chandra
+ */
+@Configuration
+public class RouterFunctionConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> route(SampleHandlerFunction sampleHandlerFunction) {
+        return RouterFunctions.route(GET("/functional/flux").and(accept(MediaType.APPLICATION_JSON)), sampleHandlerFunction::flux)
+                .andRoute(GET("/functional/mono").and(accept(MediaType.APPLICATION_JSON)), sampleHandlerFunction::mono);
+    }
+}
